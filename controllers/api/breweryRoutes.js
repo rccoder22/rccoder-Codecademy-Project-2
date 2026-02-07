aa8a6e84c91a5aaf7e788c36a5bc27d5287cf5a1
Aaron Primmer <aaronmprimmer@gmail.com>
2026-02-07 09:04:57 -0500

    Added single serach api

--- controllers/api/breweryRoutes.js
+++ controllers/api/breweryRoutes.js
@@ -1,5 +1,30 @@
 const router = require("express").Router();
 
+// POST /api/breweries/search/1
+router.post("/search/1", (req, res) => {
+  let obdId = req.body.obd_id;
+  const url = `https://api.openbrewerydb.org/v1/breweries/${obdId}`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error fetching brewery");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json({ data, url: url, status: 200 });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        status: 400,
+        message: "Error retreiving brewery info",
+        url: url,
+      });
+    });
+});
+
+// POST /api/breweries/search
 router.post("/search", (req, res) => {
   let cityEntry = req.body.city;
   cityEntry = cityEntry.trim().toLowerCase();

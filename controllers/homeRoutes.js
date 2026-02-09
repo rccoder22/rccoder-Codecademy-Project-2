58c5afc2978c488b3df5ada4ce8c02de326e179b
Aaron Primmer <aaronmprimmer@gmail.com>
2026-02-09 15:57:53 -0500

    Added login default page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const { Users, Favorites } = require("../models");
 
 router.get("/", (req, res) => {
   res.render("homePage", {});
 });
 
+router.get("/login", (req, res) => {
+  res.render("loginPage", {});
+});
 module.exports = router;

442a2874afbe55df485bd23512fee6c2ce93aa3a
Aaron Primmer <aaronmprimmer@gmail.com>
2026-02-11 08:12:03 -0500

    Added functionality to single search page and link to logo

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,40 @@
 const router = require("express").Router();
 const { Users, Favorites } = require("../models");
 
+// Home page
 router.get("/", (req, res) => {
   res.render("homePage", {});
 });
 
+//Search for one Brewery
 router.get("/search/1", (req, res) => {
-  res.render("singleSearchPage", {});
+  if (req.query.bn) {
+    const lookup = req.query.bn;
+    const url = `https://api.openbrewerydb.org/v1/breweries/${lookup}`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          res.status(400).json({
+            status: 400,
+            message: `Database response status ${response.status}`,
+          });
+          return null;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          res.render("singleSearchPage", { data });
+        }
+      })
+      .catch((error) => {
+        res.status(400).json({
+          status: 400,
+          message: error.message,
+        });
+      });
+  }
 });
 
 router.get("/favorites", async (req, res) => {

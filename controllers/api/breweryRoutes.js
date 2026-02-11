061ec30493c66efdec2eb0f5ef03e91a3256bab6
Aaron Primmer <aaronmprimmer@gmail.com>
2026-02-11 08:28:09 -0500

    Added conditions to routes

--- controllers/api/breweryRoutes.js
+++ controllers/api/breweryRoutes.js
@@ -12,59 +12,73 @@ router.post("/", (req, res) => {
 
 // POST /api/breweries/search/1
 router.post("/search/1", (req, res) => {
-  let obdId = req.body.obd_id;
-  const url = `https://api.openbrewerydb.org/v1/breweries/${obdId}`;
+  if (req.body.obd_id) {
+    let obdId = req.body.obd_id;
+    const url = `https://api.openbrewerydb.org/v1/breweries/${obdId}`;
 
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error fetching brewery");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      res.json({ data, url: url, status: 200 });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        status: 400,
-        message: "Error retreiving brewery info",
-        url: url,
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error fetching brewery");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        res.json({ data, url: url, status: 200 });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          status: 400,
+          message: "Error retreiving brewery info",
+          url: url,
+        });
       });
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: "Invalid request.",
     });
+  }
 });
 
 // POST /api/breweries/search
 router.post("/search", (req, res) => {
-  let cityEntry = req.body.city;
-  cityEntry = cityEntry.trim().toLowerCase();
-  let url = "https://api.openbrewerydb.org/v1/breweries?per_page=10&";
-  if (isNaN(Number(cityEntry))) {
-    url += `by_city=${encodeURIComponent(cityEntry)}`;
-  } else {
-    url += `by_postal=${encodeURIComponent(cityEntry)}`;
-  }
-  
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
+  if (req.body.city) {
+    let cityEntry = req.body.city;
+    cityEntry = cityEntry.trim().toLowerCase();
+    let url = "https://api.openbrewerydb.org/v1/breweries?per_page=10&";
+    if (isNaN(Number(cityEntry))) {
+      url += `by_city=${encodeURIComponent(cityEntry)}`;
+    } else {
+      url += `by_postal=${encodeURIComponent(cityEntry)}`;
+    }
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          res.status(400).json({
+            data: [],
+            message: "Error getting info from server",
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(`Data from ODB: ${data}`);
+        res.json({ data });
+      })
+      .catch((error) => {
         res.status(400).json({
-          data: [],
-          message: "Error getting info from server",
+          status: 400,
+          message: "Cannot retreive breweries info",
         });
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(`Data from ODB: ${data}`);
-      res.json({ data });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        status: 400,
-        message: "Cannot retreive breweries info",
       });
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: "Invalid request.",
     });
+  }
 });
 
 module.exports = router;
